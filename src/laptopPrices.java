d3af6510402b2e228f09db1813226370d868efc3
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-08 22:23:32 +0200

    rozpoczęcie ralizacji zadania3

--- src/laptopPrices.java
+++ src/laptopPrices.java
@@ -1,2 +1,17 @@
 public class laptopPrices {
+    public static void main(String[] args) {
+        double[] laptopPrices;
+        laptopPrices = new double[3];
+        laptopPrices[0] = 2550.55;
+        laptopPrices[1] = 4444;
+        laptopPrices[2] = 999.99;
+        if(laptopPrices[0] > laptopPrices[1]){
+            System.out.println("Laptop nr 2 jest najtańszy");
+        }else if(laptopPrices[0] <  ){
+
+        }
+    }
+
+
+
 }
