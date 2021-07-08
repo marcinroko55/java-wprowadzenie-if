b0bbd97299d74abaf2d508f3adf953d4bc77f6eb
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-08 21:31:16 +0200

    implementacja zadania3, SrodkiTransportu

--- src/SrodkiTransportu.java
+++ src/SrodkiTransportu.java
@@ -1,5 +1,15 @@
 public class SrodkiTransportu {
     public static void main(String[] args) {
-        
+        int odleglosc = 10000;
+        if(odleglosc <=300){
+            System.out.printf("Zasuwaj pieszo");
+        }else if(odleglosc < 2700){
+            System.out.println("Pojedź rowerem");
+        }else if (odleglosc < 10000){
+            System.out.println("Wykorzystaj samochód");
+        }else{
+            System.out.println("Zarezerwuj lot");
+        }
+
     }
 }
