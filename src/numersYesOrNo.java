f4d43e4a4e6b9f06a7906a760d67f95f0e363e98
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-11 18:58:26 +0200

    #7 realizacja zadania 5

--- src/numersYesOrNo.java
+++ src/numersYesOrNo.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class numersYesOrNo {
+public class numersYesOrNo {
+    public static void main(String[] args) {
+        int[] tablica = {1, 2, 5, 6, 1};
+        if ((tablica[0] * 2 + 4) < 8) {
+            System.out.println("Tak");
+
+        } else {
+            System.out.println("Nie");
+        }
+        if ((tablica[1] * 2 + 4) < 8) {
+            System.out.println("Tak");
+
+        } else {
+            System.out.println("Nie");
+
+        }
+        if ((tablica[2] * 2 + 4) < 8) {
+            System.out.println("Tak");
+
+        } else {
+            System.out.println("Nie");
+        }
+        if ((tablica[3] * 2 + 4) < 8) {
+            System.out.println("Tak");
+
+        } else {
+            System.out.println("Nie");
+        }
+        if ((tablica[4] * 2 + 4) < 8) {
+            System.out.println("Tak");
+
+        } else {
+            System.out.println("Nie");
+        }
+    }
 }
